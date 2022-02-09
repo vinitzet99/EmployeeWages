21b33dc8756d4b3ed2b86c0ac4e1173a080f31a9
vinit <vinitsxn@gmail.com>
2022-02-09 20:13:02 +0530

    Refactor: Refactored Employee Wage Calculation to Function

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -9,18 +9,27 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
+
+/**
+ * get Working Hours
+ * 
+ * @param {*}  
+ */
+function getWorkingHours(empCheck) 
+{ 
+    switch (empCheck) //case
+    { 
+        case IS_PART_TIME: 
+            return PART_TIME_HOURS; 
+        case IS_FULL_TIME: 
+            return FULL_TIME_HOURS; 
+        default: 
+            return 0; 
+    } 
+}
+
 let empHrs = 0; 
 empCheck = Math.floor(Math.random() * 10) % 3; //generate random 
-switch (empCheck) //case
-{ 
-    case IS_PART_TIME: 
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME: 
-        empHrs = FULL_TIME_HOURS;
-        break; 
-    default: 
-        empHrs = 0; 
-} 
+empHrs=getWorkingHours(empCheck); //call function
 let empWage = empHrs * WAGE_PER_HOUR; //calculate wage
-console.log("Emp Wage: " + empWage);  //log wage
+console.log("Emp Wage: " + empWage); //log wage
