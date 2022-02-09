2ec51b12f810b17f16e8bad668c24730d50f2de2
vinit <vinitsxn@gmail.com>
2022-02-09 19:59:29 +0530

    Added: Calculate Daily Wage functionality added

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -3,14 +3,24 @@
  * Created on: 09-Feb-2022
  * Employee Wages
  */
-const IS_ABSENT = 0 //declare constant
-let empCheck = Math.floor(Math.random() * 10) % 2; //generate random value
-if (empCheck == IS_ABSENT) //condition
+//constants
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2; 
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20; 
+let empHrs = 0; 
+empCheck = Math.floor(Math.random() * 10) % 3; //generate random 
+switch (empCheck) //case
 { 
-    console.log("Employee is Absent");  //Absent Message
-    return;
+    case IS_PART_TIME: 
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME: 
+        empHrs = FULL_TIME_HOURS;
+        break; 
+    default: 
+        empHrs = 0; 
 } 
-else 
-{ 
-    console. log("Employee is Present"); //Present Message
-} 
+let empWage = empHrs * WAGE_PER_HOUR; //calculate wage
+console.log("Emp Wage: " + empWage);  //log wage
