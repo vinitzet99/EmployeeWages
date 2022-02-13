3d36c76aebbd449b10518de5abd78e05d822930f
vinit <vinitsxn@gmail.com>
2022-02-13 15:03:30 +0530

    Added: Gender and Start Date added in Employee data

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -8,13 +8,18 @@ class EmployeePayrollData{
   // property
   id;
   salary;                   
+  gender; 
+  startDate;
+ 
   // constructor
-  constructor(id, name, salary) { 
-   this.id = id;
-   this.name = name;
-   this.salary = salary;
-  }
-  
+  constructor(...params) { 
+      this.id = params[0];
+      this.name = params[1]; 
+      this.salary = params[2]; 
+      this.gender = params[3]; 
+      this.startDate = params[4];
+      }
+        
   // getter and setter method
   get name() 
   { 
@@ -28,13 +33,20 @@ class EmployeePayrollData{
   
   // method
   toString() {
-       return "id=" + this.id + ", name='" + this.name + "', salary=" + this.salary;
-   }
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      const empDate = this.startDate === undefined ? "undefined" :
+               this.startDate.toLocaleDateString("en-US", options);
+      return "id=" + this.id + ", name='" + this.name + ", salary=" + this.salary + ", "+ 
+          "gender=" + this.gender + ", startDate=" + empDate;
+    
   }
-  
-  //create object
-  let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000); 
-  console.log(employeePayrollData.toString());
-  //log name
-  employeePayrollData.name = "john";
-  console.log(employeePayrollData.toString());
+}
+ 
+//create object
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000); 
+console.log(employeePayrollData.toString());
+employeePayrollData.name = "john";
+console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date()); 
+console.log(newEmployeePayrollData.toString());
+ 
